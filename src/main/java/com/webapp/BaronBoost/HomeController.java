c5b0f096dddd4f8ac15f90509bec460e9716c8bf
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2024-07-06 03:06:15 -0400

    clean up

--- src/main/java/com/webapp/BaronBoost/HomeController.java
+++ src/main/java/com/webapp/BaronBoost/HomeController.java
@@ -1,20 +1,12 @@
 package com.webapp.BaronBoost;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @Controller
 public class HomeController {
-
-
-    @GetMapping("/email-verification/{code}")
-    public String emailVerification(@PathVariable(name = "code") String code, Model model) {
-        model.addAttribute("message", "Email verification successful!");
-        return "email-verification";
-    }
 
     @GetMapping("/")
     public String home() {
